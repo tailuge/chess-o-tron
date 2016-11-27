63c01984fe2d840ff7b1e4efba32777179cd270c
TaiLuGe <tailuge@gmail.com>
2016-11-27 08:27:46 +0000

    extract modules

---
+++ generate/chessExt.js
@@ -0,0 +1,98 @@
+/**
+ * Chess extensions
+ */
+
+var Chess = require('./lib/chess').Chess;
+
+var allSquares = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'];
+
+/**
+ * Place king at square and find out if it is in check.
+ */
+function isCheckAfterPlacingKingAtSquare(fen, king, square) {
+    var chess = new Chess();
+    chess.load(fen);
+    chess.remove(square);
+    chess.remove(king);
+    chess.put({
+        type: 'k',
+        color: chess.turn()
+    }, square);
+    return chess.in_check();
+}
+
+/**
+ * Switch side to play (and remove en-passent information)
+ */ 
+function fenForOtherSide(fen) {
+    return fen.replace(/ w .*/, " b - - 0 1");
+}
+
+/**
+ * Where is the kings
+ */ 
+function kingsSquare(fen, colour) {
+    var chess = new Chess();
+    chess.load(fen);
+    return allSquares.find(square => {
+        var r = chess.get(square);
+        return r === null ? false : (r.color == colour && r.type.toLowerCase() === 'k');
+    });
+}
+
+/**
+ * Find position of all of one colours pieces excluding the king.
+ */
+function piecesForColour(fen, colour) {
+    var chess = new Chess();
+    chess.load(fen);
+    return allSquares.filter(square => {
+        var r = chess.get(square);
+        if ((r === null) || (r.type === 'k')) {
+            return false;
+        }
+        return r.color == colour;
+    });
+}
+
+/**
+ * Convert PGN to list of FENs.
+ */
+function pgnToFens(pgn) {
+    var gameMoves = pgn.replace(/([0-9]+\.\s)/gm, '').trim();
+    var moveArray = gameMoves.split(' ').filter(function (n) {
+        return n;
+    });
+
+    var fens = [];
+    var chess = new Chess();
+    moveArray.forEach(move => {
+        chess.move(move, {
+            sloppy: true
+        });
+
+        // skip opening moves
+        if (chess.history().length < 8) {
+            return;
+        }
+
+        // skip positions in check
+        if (chess.in_check()) {
+            return;
+        }
+
+        // skip black moves
+        if (chess.turn() === 'b') {
+            return;
+        }
+        fens.push(chess.fen());
+    });
+    return fens;
+}
+
+module.exports.allSquares= allSquares;
+module.exports.pgnToFens = pgnToFens;
+module.exports.kingsSquare = kingsSquare;
+module.exports.piecesForColour = piecesForColour;
+module.exports.isCheckAfterPlacingKingAtSquare = isCheckAfterPlacingKingAtSquare;
+module.exports.fenForOtherSide = fenForOtherSide;
--- generate/generatePuzzles.js
+++ generate/generatePuzzles.js
@@ -11,7 +11,9 @@
  */
 
 var Chess = require('./lib/chess').Chess;
+var ChessExt = require('./chessExt');
 
+console.log(ChessExt.allSquares);
 
 /**
  * Read from stdin and apply puzzle generator to each line
@@ -29,7 +31,7 @@ rl.on('close', function () {
 });
 
 rl.on('line', function (pgnLine) {
-    pgnToFens(pgnLine)
+    ChessExt.pgnToFens(pgnLine)
         .map(generate)
         .filter(filterForChanges)
         .forEach(puzzle => console.log(JSON.stringify(puzzle) + ","));
@@ -70,41 +72,6 @@ function enrichWithPiecesThatCanGiveCheck(puzzle) {
 
 
 
-/**
- * Convert PGN to list of FENs.
- */
-function pgnToFens(pgn) {
-    var gameMoves = pgn.replace(/([0-9]+\.\s)/gm, '').trim();
-    var moveArray = gameMoves.split(' ').filter(function (n) {
-        return n;
-    });
-
-    var fens = [];
-    var chess = new Chess();
-    moveArray.forEach(move => {
-        chess.move(move, {
-            sloppy: true
-        });
-
-        // skip opening moves
-        if (chess.history().length < 8) {
-            return;
-        }
-
-        // skip positions in check
-        if (chess.in_check()) {
-            return;
-        }
-
-        // skip black moves
-        if (chess.turn() === 'b') {
-            return;
-        }
-        fens.push(chess.fen());
-    });
-    return fens;
-}
-
 function filterForChanges(elt, index, array) {
     if (index === 0) {
         return true;
@@ -113,31 +80,9 @@ function filterForChanges(elt, index, array) {
     return (JSON.stringify(array[index].targetPieces) != JSON.stringify(array[index - 1].targetPieces));
 }
 
-var allSquares = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'];
 
-function kingsSquare(fen, colour) {
-    var chess = new Chess();
-    chess.load(fen);
-    return allSquares.find(square => {
-        var r = chess.get(square);
-        return r === null ? false : (r.color == colour && r.type.toLowerCase() === 'k');
-    });
-}
 
-/**
- * Find position of all of one colours pieces excluding the king.
- */
-function piecesForColour(fen, colour) {
-    var chess = new Chess();
-    chess.load(fen);
-    return allSquares.filter(square => {
-        var r = chess.get(square);
-        if ((r === null) || (r.type === 'k')) {
-            return false;
-        }
-        return r.color == colour;
-    });
-}
+
 
 function loosePiecesForBothSides(fen) {
     var blackLoosePieces = loosePieces(fen);
@@ -148,8 +93,8 @@ function loosePiecesForBothSides(fen) {
 function loosePieces(fen) {
     var chess = new Chess();
     chess.load(fen);
-    var king = kingsSquare(fen, chess.turn());
-    var pieces = piecesForColour(fen, chess.turn() == 'w' ? 'b' : 'w');
+    var king = ChessExt.kingsSquare(fen, chess.turn());
+    var pieces = ChessExt.piecesForColour(fen, chess.turn() == 'w' ? 'b' : 'w');
     return pieces.filter(square => !isCheckAfterPlacingKingAtSquare(fen, king, square));
 }
 
