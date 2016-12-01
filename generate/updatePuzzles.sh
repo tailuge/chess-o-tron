24cf01b52c30ca04d8118fbcef5995275018a652
TaiLuGe <tailuge@gmail.com>
2016-12-01 11:20:39 +0000

    regenerate puzzles

---
+++ generate/updatePuzzles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "var problems=[" > ../html/js/feature-tron-data.js
+cat raw.puzzles | \
+    node absolutePin.js | \
+    node loosePieces.js | \
+    node checkingSquares.js | \
+    sed 's/$/,/' >> ../html/js/feature-tron-data.js
+echo "];" >> ../html/js/feature-tron-data.js
+
+wc -l ../html/js/feature-tron-data.js
+
