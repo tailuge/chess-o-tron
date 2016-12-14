fcff43ea47eb2f4a39d69c3d6ac2e5ed2afae015
TaiLuGe <tailuge@gmail.com>
2016-12-14 20:56:49 +0000

    generate diagrams from fen

--- ui/src/main.js
+++ ui/src/main.js
@@ -14,6 +14,6 @@ function main(opts) {
 
 main({
     element: document.getElementById("wrapper"),
-    //fen: "b3k2r/1p3pp1/5p2/5n2/8/5N2/6PP/5K1R w - -"
-    fen: "qn1r1k2/2r1b1np/pp1pQ1p1/3P2P1/1PP2P2/7R/PB4BP/4R1K1 w - -"
+    fen: "b3k2r/1p3pp1/5p2/5n2/8/5N2/6PP/5K1R w - -"
+    //fen: "qn1r1k2/2r1b1np/pp1pQ1p1/3P2P1/1PP2P2/7R/PB4BP/4R1K1 w - -"
 });
