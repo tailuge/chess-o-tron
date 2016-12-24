b0f7398fdb767c45096480a5443ec20b30692eca
TaiLuGe <tailuge@gmail.com>
2016-12-24 15:45:38 +0000

    link to training page

--- quiz/src/view/selection.js
+++ quiz/src/view/selection.js
@@ -2,7 +2,7 @@ var m = require('mithril');
 
 module.exports = function(controller) {
   return [
-    m('select', [
+    m('select.selectblack', [
       m('option', {
         value: "Knight forks"
       }, "Knight forks"),
