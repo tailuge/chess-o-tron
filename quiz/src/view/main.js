d8e163a45080ce9ee814f05c9f209804a4ebee02
TaiLuGe <tailuge@gmail.com>
2016-12-29 16:53:29 +0000

    link to forum

--- quiz/src/view/main.js
+++ quiz/src/view/main.js
@@ -18,7 +18,7 @@ function info(ctrl) {
     m('ul.instructions', [
       m('li.instructions', 'Find all knight forking squares.'),
       m('li.instructions', 'Break bonus by clicking quickly.'),
-      m('li.instructions', 'Post your high score on ', m("a.hiscore.external[href='https://en.lichess.org/forum/game-analysis/altanalysis']", {
+      m('li.instructions', 'Post your high score on ', m("a.hiscore.external[href='https://en.lichess.org/forum/game-analysis/forking-hell-challenge']", {
         style: {
           color: "#55a"
         }
