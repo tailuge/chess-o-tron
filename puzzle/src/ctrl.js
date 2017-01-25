5cc323c78ba9cc5effb5ded12c2dde9779deae37
TaiLuGe <tailuge@gmail.com>
2017-01-25 21:10:35 +0000

    clear out old code

--- puzzle/src/ctrl.js
+++ puzzle/src/ctrl.js
@@ -1,26 +1,19 @@
 var m = require('mithril');
 var groundBuild = require('./ground');
-var generate = require('../../generate/src/generate');
-var diagram = require('../../generate/src/diagram');
 
 
 module.exports = function(opts, i18n) {
 
-  var betweenFens = false;
-  var gameTotal = 40;
   var selection = m.prop(opts.mode);
-  var fen = m.prop(opts.fen ? opts.fen : generate.randomFenForFeature(selection()));
+  var fen = m.prop();
   var fenForBoard = fen();
-  var features = m.prop(generate.extractSingleFeature(selection(), fen()));
 
-  var randomFeature;
   var ground;
   var score = m.prop();
   var displayscore = m.prop();
   var beatbonus = m.prop();
   var offbeatbonus = m.prop();
-  var correct = m.prop([]);
-  var incorrect = m.prop([]);
+
   var timerId;
 
   function showGround() {
@@ -32,9 +25,6 @@ module.exports = function(opts, i18n) {
     score(0);
     displayscore(0);
 
-    correct([]);
-    incorrect([]);
-    nextFen();
     if (!timerId) {
       timerId = setInterval(onTick, 25);
     }
@@ -45,7 +35,7 @@ module.exports = function(opts, i18n) {
 
     var t = new Date().getTime() / 1000;
     var radians = t * 2 * Math.PI;
-    var bpm = 26.5;
+    var bpm = 26.7;
     var bps = bpm / 60;
     var beat = Math.sin(radians * bps);
     beatbonus(50 + beat * 50);
@@ -58,75 +48,9 @@ module.exports = function(opts, i18n) {
   }
 
   function onSquareSelect(target) {
-    if (betweenFens) {
-      return;
-    }
-    if (correct().includes(target) || incorrect().includes(target)) {
-      target = 'none';
-    }
-    else {
-      var found = generate.featureFound(features(), target);
-      if (found > 0) {
-        correct().push(target);
-      }
-      else {
-        incorrect().push(target);
-        score(score() - 1);
-      }
-    }
-    ground.set({
-      fen: fenForBoard,
-    });
-    var clickedDiagram = diagram.clickedSquares(features(), correct(), incorrect(), target);
-    ground.setShapes(clickedDiagram);
-    m.redraw();
-    if (generate.allFeaturesFound(features())) {
 
-    }
   }
 
-  function gameOver() {
-    m.redraw();
-  }
-
-  function updateFen(value) {
-    diagram.clearDiagrams(features());
-    fen(value);
-    fenForBoard = fen();
-    ground.set({
-      fen: fenForBoard,
-    });
-    ground.setShapes([]);
-    correct([]);
-    incorrect([]);
-
-    var feature = selection() === 'Mixed' ? randomFeature : selection();
-
-    features(generate.extractSingleFeature(feature, fen()));
-    if (generate.allFeaturesFound(features())) {
-      return nextFen();
-    }
-    m.redraw();
-  }
-
-  function nextFen() {
-    randomFeature = generate.randomFeature();
-    var feature = selection() === 'Mixed' ? randomFeature : selection();
-    updateFen(generate.randomFenForFeature(feature));
-  }
-
-  function blindfold() {
-    if (fenForBoard === '8/8/8/8/8/8/8/8') {
-      fenForBoard = fen();
-    }
-    else {
-      fenForBoard = '8/8/8/8/8/8/8/8';
-    }
-    ground.set({
-      fen: fenForBoard,
-    });
-    m.redraw();
-  }
 
   showGround();
   newGame();
@@ -135,17 +59,12 @@ module.exports = function(opts, i18n) {
   return {
     fen: fen,
     ground: ground,
-    features: features,
-    updateFen: updateFen,
     onSquareSelect: onSquareSelect,
-    nextFen: nextFen,
     score: score,
     displayscore: displayscore,
     beatbonus: beatbonus,
     offbeatbonus: offbeatbonus,
     selection: selection,
-    newGame: newGame,
-    blindfold: blindfold,
-    descriptions: generate.featureMap.map(f => f.description)
+    newGame: newGame
   };
 };
