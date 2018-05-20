620b911a45588ce6857b9399f04f4b36c707fa10
tailuge <tailuge@gmail.com>
2018-05-20 14:41:33 +0000

    use new lichess API

--- public/openings/trie-data.js
+++ public/openings/trie-data.js
@@ -9,10 +9,10 @@ var dataCache = {}
 
 function fetchLichessData(player, accumulated, pages, cacheid, callback) {
 
-	status("Fetching data for player: " + player + " page: " + pages[0]);
+	status("Fetching last " + pages + " games of player: " + player + " ... at 10 games per second");
 
 	if (dataCache[cacheid]) {
-		status("Using cached data for player: " + player + " page: " + pages[0] + " calculating");
+		status("Using cached data for player: " + player);
 		setTimeout(function() {
 			callback(dataCache[cacheid]);
 		}, 100);
@@ -20,23 +20,19 @@ function fetchLichessData(player, accumulated, pages, cacheid, callback) {
 	}
 
 	$.ajax({
-		url: "https://en.lichess.org/api/user/" + player + "/games?nb=100&with_analysis=1&with_moves=1&with_opening=1&page=" + pages.shift(),
-		dataType: 'json',
+		url: "https://lichess.org/games/export/" + player + "?max=" + pages + "&evals=true&moves=true&opening=true",
+		headers: {
+			Accept: "application/x-ndjson"
+		},
 		cache: false,
 		success: function(data) {
-			var all = accumulated.concat(data.currentPageResults);
-			if (pages.length > 0) {
-				setTimeout(function() {
-					fetchLichessData(player, all, pages, cacheid, callback);
-				}, 1500);
-			}
-			else {
-				dataCache[cacheid] = all;
-				status("Calculating");
-				setTimeout(function() {
-					callback(all);
-				}, 100);
-			}
+			data = data.split("\n").filter(x => x !== "").map(x => JSON.parse(x));
+			console.log(data);
+			dataCache[cacheid] = data;
+			status("Calculating");
+			setTimeout(function() {
+				callback(data);
+			}, 100);
 		}.bind(this),
 		error: function(xhr, status, err) {
 			console.error(this.props.url, status, err.toString());
@@ -44,33 +40,41 @@ function fetchLichessData(player, accumulated, pages, cacheid, callback) {
 	});
 }
 
+// curl 'https://lichess.org/games/export/tailuge?max=2' -H 'Accept: application/x-ndjson' 
+
 function processData(allgames, player, colour, filter, trim, depth, variant, timecontrol) {
-	console.log(JSON.stringify(allgames,null,1));
+	console.log(JSON.stringify(allgames, null, 1));
 	variant = variant ? variant : 'standard'
+	console.log("allgames:", allgames.length);
 
 	var gamesWithStandardVariant = allgames.filter(x => x.variant === variant);
+	console.log("gamesWithStandardVariant:", gamesWithStandardVariant.length);
 	var gamesWithTimecontrol = gamesWithStandardVariant.filter(x => timecontrol ? x.speed === timecontrol : true);
+	console.log("gamesWithTimecontrol:", gamesWithTimecontrol.length);
 	var gamesByPlayer = gamesWithTimecontrol.filter(x => {
-		var id = colour === "white" ? x.players.white.userId : x.players.black.userId;
+		var id = colour === "white" ? x.players.white.user.id : x.players.black.user.id;
 		return id && id.toUpperCase() == player.toUpperCase();
 	});
+	console.log("gamesByPlayer:", gamesByPlayer.length);
+
 	var regExp = new RegExp("^" + filter + ".*$");
 	var gamesWithRegEx = gamesByPlayer.filter(x => x.moves.length > 4 && x.moves.match(regExp));
 	if (depth === undefined) {
 		depth = 24;
-	};
+	}
+	console.log("gamesWithRegEx:", gamesWithRegEx.length);
 	var uniqPrefixGames = gamesWithRegEx;
 	var games = uniqPrefixGames.map(x => {
-		var url = x.url.replace('white', colour).replace('black', colour);
+		var url = "https://lichess.org/" + x.id + "/" + colour;
 		var score = '{0.5,0.5}';
 		if (x.winner) {
 			score = x.winner === colour ? '{1,0}' : '{0,1}';
 		}
 		var opening = "";
 		if (x.opening && x.opening.name) {
-			opening = "_" + x.opening.name.replace(/ /g,"_");
+			opening = "_" + x.opening.name.replace(/ /g, "_");
 		}
-	
+
 		return "start " + x.moves.split(" ").slice(0, depth).join(" ") + "..." + url + score + opening;
 	});
 
@@ -91,10 +95,10 @@ function processData(allgames, player, colour, filter, trim, depth, variant, tim
 	var d3Links = nodesToLinks(nodes);
 	status("propagate scores");
 	backPropagateScores(nodes);
-//	console.log(JSON.stringify(nodes));
+	//	console.log(JSON.stringify(nodes));
 
 	var d3Nodes = nodes.map(t => textToNode(t, evalDictionary));
-//	console.log(JSON.stringify(d3Nodes));
+	//	console.log(JSON.stringify(d3Nodes));
 
 	status("Produced " + nodes.length + " nodes from " + games.length + " games");
 
